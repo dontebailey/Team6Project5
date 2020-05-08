eaac065692b60a999c78a0e15a618ee48c010078
dontebailey <baileydonte27@gmail.com>
2020-05-08 12:01:59 -0400

    First attempt with controller model

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,6 +1,8 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -18,10 +20,32 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = null;
         var loc = getClass().getResource("Main.fxml");
-        root = FXMLLoader.load(loc);
+        try{
+            root = FXMLLoader.load(loc);
+        } catch(IOException e){
+            System.out.println("Couldn't find FXML file");
+        }
         Scene windowContents = new Scene(root);
         primaryStage.setScene(windowContents);
         primaryStage.show();
 
     }
+
+
+    @FXML
+    public void openChuckNorris(ActionEvent event){
+        Parent ChuckRoot = null;
+        var loc = getClass().getResource("ChuckNorris.fxml");
+        try{
+            ChuckRoot = FXMLLoader.load(loc);
+        } catch(IOException e){
+            System.out.println("Couldn't find FXML file");
+        }
+        Scene windowContents = new Scene(ChuckRoot);
+        Stage chuckWindow = new Stage();
+        chuckWindow.setScene(windowContents);
+        chuckWindow.show();
+    }
+
+
 }
