f5b059c8a666c40b6d91c2ea14c9a6f42f98a182
showe93 <s2howe@student.bridgew.edu>
2020-05-10 23:37:11 -0400

    Data Handler Updated

--- src/main/java/bsu/comp152/DataHandler.java
+++ src/main/java/bsu/comp152/DataHandler.java
@@ -97,4 +97,59 @@ public class DataHandler {
             return "Song Title: " + song_name ;  //only title for display later
         }
     }
+    public ArrayList<ChuckNorrisModel.jokeCategories> getChuckData(){
+        var requestBuilder = HttpRequest.newBuilder();
+        var dataRequest = requestBuilder.uri(URI.create(webLocation)).build();
+        HttpResponse<String> response = null;
+        try {
+            response = dataGrabber.send(dataRequest, HttpResponse.BodyHandlers.ofString());
+        }catch(IOException e){
+            System.out.println("Error connecting to network or site");
+        }
+        catch (InterruptedException e){
+            System.out.println("Connection to site broken");
+        }
+        if (response == null ){
+            System.out.println("Something went terribly wrong, ending program");
+            System.exit(-1);
+        }
+        var usefulData = response.body();
+        var jsonInterpreter = new Gson();
+        var jokeData = jsonInterpreter.fromJson(usefulData, ChuckNorrisModel.jokeDataType.class);
+        //System.out.println(recipeData.results);
+        return jokeData.categories;
+
+    }
+
+    class jokeDataType{
+        ArrayList<ChuckNorrisModel.jokeCategories> categories;
+        String value;
+
+        @Override
+        public String toString(){
+            return value;
+        }
+    }
+
+    class jokeCategories{
+        String animal;
+        String career;
+        String celebrity;
+        String fashion;
+        String food;
+        String history;
+        String money;
+        String movie;
+        String sport;
+        String travel;
+
+
+    }
+
+
+
+
+
+
+
 }
